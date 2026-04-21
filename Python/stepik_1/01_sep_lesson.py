bdfd0b27fcc94296b81648866ad1a1d1eab25878
tclya8 <tclya8@gmail.com>
2026-04-21 10:44:28 +0300

    Test changes

--- Python/stepik_1/01_sep_lesson.py
+++ Python/stepik_1/01_sep_lesson.py
@@ -2,4 +2,4 @@ razdel = input()
 s1 = input()
 s2 = input()
 s3 = input()
-print(s1, s2, s3, sep=razdel)
+print(s1, s2, s3, sep=razdel)  # 1
--- Python/stepik_1/08_.py
+++ Python/stepik_1/08_.py
@@ -1,4 +1,4 @@
-s1 = int(input())
+s1 = int(input())       # 2
 s2 = int(input())
 s3 = int(input())
 i = s1 + s2 * (s3 - 1)
